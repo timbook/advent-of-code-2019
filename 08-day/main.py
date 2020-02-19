6cd103f9c11c49ce7080771145f6995ebf0425b2
Tim Book <timothykbook@gmail.com>
2020-02-18 22:37:55 -0500

    day 8 part 2

--- 08-day/main.py
+++ 08-day/main.py
@@ -15,5 +15,30 @@ subarr = data[min_layer, : , :]
 n_ones = np.sum(subarr == 1)
 n_twos = np.sum(subarr == 2)
 
-print("SOLUTION:")
+print("=== PART A ===")
 print(n_ones * n_twos)
+print()
+
+#==============================================================================
+
+# 0 = Black
+# 1 = White
+# 2 = Transparent
+
+def get_num(arr):
+    for val in arr:
+        if val == 2:
+            continue
+        else:
+            return val
+
+res = np.apply_along_axis(get_num, 0, data)
+
+import matplotlib.pyplot as plt
+plt.imshow(res, cmap="Greys_r")
+plt.show()
+
+
+
+
+
